e3ccf62391fe91f97ccf71aca7e8f0a95e31d1bd
Paul Riseborough <gncsolns@gmail.com>
2025-04-13 14:20:40 +1000

    AP_TECS: Reset current throttle when setting minimum value
    This prevents a single frame glitch in throttle that can result if the TECS throttle is fetched before the next TECS update.

--- libraries/AP_TECS/AP_TECS.cpp
+++ libraries/AP_TECS/AP_TECS.cpp
@@ -1374,6 +1374,7 @@ void AP_TECS::set_throttle_min(const float thr_min, bool reset_output) {
         _THRminf_ext = thr_min;
         if (reset_output) {
             _last_throttle_dem = MAX(_last_throttle_dem, _THRminf_ext);
+            _throttle_dem = _last_throttle_dem;
         }
     }
 }
