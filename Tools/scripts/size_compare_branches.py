ArduPilot/ardupilot

Tools: add option to compare features to size_compare_branches.py

Tools/scripts/size_compare_branches.py
@@ -41,6 +41,16 @@ class SizeCompareBranchesResult(object):
         self.identical = identical
 
 
+class FeatureCompareBranchesResult(object):
+    '''object to return results from a comparison'''
+
+    def __init__(self, board, vehicle, delta_features_in, delta_features_out):
+        self.board = board
+        self.vehicle = vehicle
+        self.delta_features_in = delta_features_in
+        self.delta_features_out = delta_features_out
+
+
 class SizeCompareBranches(object):
     '''script to build and compare branches using elf_diff'''
 
@@ -62,7 +72,9 @@ class SizeCompareBranches(object):
                  extra_hwdef_branch=[],
                  extra_hwdef_master=[],
                  parallel_copies=None,
-                 jobs=None):
+                 jobs=None,
+                 compare='size',
+                 ):
 
         if branch is None:
             branch = self.find_current_git_branch_or_sha1()
@@ -84,6 +96,7 @@ class SizeCompareBranches(object):
         self.show_unchanged = show_unchanged
         self.parallel_copies = parallel_copies
         self.jobs = jobs
+        self.compare = compare
 
         if self.bin_dir is None:
             self.bin_dir = self.find_bin_dir()
@@ -600,8 +613,38 @@ class SizeCompareBranches(object):
 
             self.run_program("SCB", elf_diff_commandline)
 
+    def compare_task_results_features(self, task_results, no_elf_diff=False):
+        pairs = {}
+        for res in task_results:
+            board = res["board"]
+            if board not in pairs:
+                pairs[board] = {}
+            if res["branch"] == self.master_commit:
+                pairs[board]["master"] = res
+            elif res["branch"] == self.branch:
+                pairs[board]["branch"] = res
+            else:
+                raise ValueError(res["branch"])
+
+        results = {}
+        for pair in pairs.values():
+            if "master" not in pair or "branch" not in pair:
+                # probably incomplete:
+                continue
+            master = pair["master"]
+            board = master["board"]
+            try:
+                results[board] = self.compare_results(master, pair["branch"])
+            except FileNotFoundError:
+                pass
+
+        return results
+
     def compare_task_results(self, task_results, no_elf_diff=False):
         # pair off results, master and branch:
+        if self.compare == 'features':
+            return self.compare_task_results_features(task_results)
+
         pairs = {}
         for res in task_results:
             board = res["board"]
@@ -647,14 +690,18 @@ class SizeCompareBranches(object):
             line = [board]
             board_results = results[board]
             for vehicle in sorted_all_vehicles:
-                bytes_delta = ""
+                cell_value = ""
                 if vehicle in board_results:
                     result = board_results[vehicle]
-                    if result.identical:
-                        bytes_delta = "*"
+                    if isinstance(result, FeatureCompareBranchesResult):
+                        cell_value = '"' + "\n".join(result.delta_features_in + result.delta_features_out) + '"'
                     else:
-                        bytes_delta = result.bytes_delta
-                line.append(str(bytes_delta))
+                        if result.identical:
+                            bytes_delta = "*"
+                        else:
+                            bytes_delta = result.bytes_delta
+                        cell_value = bytes_delta
+                line.append(str(cell_value))
             # do not add to ret value if we're not showing empty results:
             if not self.show_empty:
                 if len(list(filter(lambda x : x != "", line[1:]))) == 0:
@@ -746,7 +793,55 @@ class SizeCompareBranches(object):
 
         return result
 
+    def get_features(self, path):
+        from extract_features import ExtractFeatures
+        x = ExtractFeatures(path)
+        return x.extract()
+
+    def compare_results_features(self, result_master, result_branch):
+        ret = {}
+        for vehicle in result_master["vehicle"].keys():
+            # check for the difference in size (and identicality)
+            # of the two binaries:
+            master_bin_dir = result_master["vehicle"][vehicle]["bin_dir"]
+            new_bin_dir = result_branch["vehicle"][vehicle]["bin_dir"]
+
+            elf_filename = result_master["vehicle"][vehicle]["elf_filename"]
+            master_path = os.path.join(master_bin_dir, elf_filename)
+            new_path = os.path.join(new_bin_dir, elf_filename)
+
+            if not os.path.exists(master_path):
+                raise FileNotFoundError(master_path)
+            if not os.path.exists(new_path):
+                raise FileNotFoundError(new_path)
+            (master_features_in, master_features_out) = self.get_features(master_path)
+            (new_features_in, new_features_out) = self.get_features(new_path)
+
+            board = result_master["board"]
+            in_delta = []
+            for master_feature_in in sorted(master_features_in):
+                if master_feature_in not in new_features_in:
+                    in_delta.append("-" + master_feature_in)
+            for new_feature_in in sorted(new_features_in):
+                if new_feature_in not in master_features_in:
+                    in_delta.append("+" + new_feature_in)
+
+            out_delta = []
+            for master_feature_out in sorted(master_features_out):
+                if master_feature_out not in new_features_out:
+                    out_delta.append("-!" + master_feature_out)
+            for new_feature_out in sorted(new_features_out):
+                if new_feature_out not in master_features_out:
+                    out_delta.append("+!" + new_feature_out)
+
+            ret[vehicle] = FeatureCompareBranchesResult(board, vehicle, in_delta, out_delta)
+
+        return ret
+
     def compare_results(self, result_master, result_branch):
+        if self.compare == 'features':
+            return self.compare_results_features(result_master, result_branch)
+
         ret = {}
         for vehicle in result_master["vehicle"].keys():
             # check for the difference in size (and identicality)
@@ -848,6 +943,11 @@ if __name__ == '__main__':
                       action="append",
                       help="exclude any board which matches this pattern")
     parser.add_option("",
+                      "--compare",
+                      default="size",
+                      choices=['size', 'features'],
+                      help="what sort of comparison to do")
+    parser.add_option("",
                       "--all-vehicles",
                       action='store_true',
                       default=False,
@@ -894,5 +994,6 @@ if __name__ == '__main__':
         show_unchanged=not cmd_opts.hide_unchanged,
         parallel_copies=cmd_opts.parallel_copies,
         jobs=cmd_opts.jobs,
+        compare=cmd_opts.compare,
     )
     x.run()

SRAravind2410/drone_code

Tools/scripts/size_compare_branches.py
@@ -41,6 +41,16 @@ class SizeCompareBranchesResult(object):
         self.identical = identical
 
 
+class FeatureCompareBranchesResult(object):
+    '''object to return results from a comparison'''
+
+    def __init__(self, board, vehicle, delta_features_in, delta_features_out):
+        self.board = board
+        self.vehicle = vehicle
+        self.delta_features_in = delta_features_in
+        self.delta_features_out = delta_features_out
+
+
 class SizeCompareBranches(object):
     '''script to build and compare branches using elf_diff'''
 
@@ -62,7 +72,9 @@ class SizeCompareBranches(object):
                  extra_hwdef_branch=[],
                  extra_hwdef_master=[],
                  parallel_copies=None,
-                 jobs=None):
+                 jobs=None,
+                 compare='size',
+                 ):
 
         if branch is None:
             branch = self.find_current_git_branch_or_sha1()
@@ -84,6 +96,7 @@ class SizeCompareBranches(object):
         self.show_unchanged = show_unchanged
         self.parallel_copies = parallel_copies
         self.jobs = jobs
+        self.compare = compare
 
         if self.bin_dir is None:
             self.bin_dir = self.find_bin_dir()
@@ -600,8 +613,38 @@ class SizeCompareBranches(object):
 
             self.run_program("SCB", elf_diff_commandline)
 
+    def compare_task_results_features(self, task_results, no_elf_diff=False):
+        pairs = {}
+        for res in task_results:
+            board = res["board"]
+            if board not in pairs:
+                pairs[board] = {}
+            if res["branch"] == self.master_commit:
+                pairs[board]["master"] = res
+            elif res["branch"] == self.branch:
+                pairs[board]["branch"] = res
+            else:
+                raise ValueError(res["branch"])
+
+        results = {}
+        for pair in pairs.values():
+            if "master" not in pair or "branch" not in pair:
+                # probably incomplete:
+                continue
+            master = pair["master"]
+            board = master["board"]
+            try:
+                results[board] = self.compare_results(master, pair["branch"])
+            except FileNotFoundError:
+                pass
+
+        return results
+
     def compare_task_results(self, task_results, no_elf_diff=False):
         # pair off results, master and branch:
+        if self.compare == 'features':
+            return self.compare_task_results_features(task_results)
+
         pairs = {}
         for res in task_results:
             board = res["board"]
@@ -647,14 +690,18 @@ class SizeCompareBranches(object):
             line = [board]
             board_results = results[board]
             for vehicle in sorted_all_vehicles:
-                bytes_delta = ""
+                cell_value = ""
                 if vehicle in board_results:
                     result = board_results[vehicle]
-                    if result.identical:
-                        bytes_delta = "*"
+                    if isinstance(result, FeatureCompareBranchesResult):
+                        cell_value = '"' + "\n".join(result.delta_features_in + result.delta_features_out) + '"'
                     else:
-                        bytes_delta = result.bytes_delta
-                line.append(str(bytes_delta))
+                        if result.identical:
+                            bytes_delta = "*"
+                        else:
+                            bytes_delta = result.bytes_delta
+                        cell_value = bytes_delta
+                line.append(str(cell_value))
             # do not add to ret value if we're not showing empty results:
             if not self.show_empty:
                 if len(list(filter(lambda x : x != "", line[1:]))) == 0:
@@ -746,7 +793,55 @@ class SizeCompareBranches(object):
 
         return result
 
+    def get_features(self, path):
+        from extract_features import ExtractFeatures
+        x = ExtractFeatures(path)
+        return x.extract()
+
+    def compare_results_features(self, result_master, result_branch):
+        ret = {}
+        for vehicle in result_master["vehicle"].keys():
+            # check for the difference in size (and identicality)
+            # of the two binaries:
+            master_elf_dir = result_master["vehicle"][vehicle]["elf_dir"]
+            new_elf_dir = result_branch["vehicle"][vehicle]["elf_dir"]
+
+            elf_filename = result_master["vehicle"][vehicle]["elf_filename"]
+            master_path = os.path.join(master_elf_dir, elf_filename)
+            new_path = os.path.join(new_elf_dir, elf_filename)
+
+            if not os.path.exists(master_path):
+                continue
+            if not os.path.exists(new_path):
+                continue
+            (master_features_in, master_features_out) = self.get_features(master_path)
+            (new_features_in, new_features_out) = self.get_features(new_path)
+
+            board = result_master["board"]
+            in_delta = []
+            for master_feature_in in sorted(master_features_in):
+                if master_feature_in not in new_features_in:
+                    in_delta.append("-" + master_feature_in)
+            for new_feature_in in sorted(new_features_in):
+                if new_feature_in not in master_features_in:
+                    in_delta.append("+" + new_feature_in)
+
+            out_delta = []
+            for master_feature_out in sorted(master_features_out):
+                if master_feature_out not in new_features_out:
+                    out_delta.append("-!" + master_feature_out)
+            for new_feature_out in sorted(new_features_out):
+                if new_feature_out not in master_features_out:
+                    out_delta.append("+!" + new_feature_out)
+
+            ret[vehicle] = FeatureCompareBranchesResult(board, vehicle, in_delta, out_delta)
+
+        return ret
+
     def compare_results(self, result_master, result_branch):
+        if self.compare == 'features':
+            return self.compare_results_features(result_master, result_branch)
+
         ret = {}
         for vehicle in result_master["vehicle"].keys():
             # check for the difference in size (and identicality)
@@ -848,6 +943,11 @@ if __name__ == '__main__':
                       action="append",
                       help="exclude any board which matches this pattern")
     parser.add_option("",
+                      "--compare",
+                      default="size",
+                      choices=['size', 'features'],
+                      help="what sort of comparison to do")
+    parser.add_option("",
                       "--all-vehicles",
                       action='store_true',
                       default=False,
@@ -894,5 +994,6 @@ if __name__ == '__main__':
         show_unchanged=not cmd_opts.hide_unchanged,
         parallel_copies=cmd_opts.parallel_copies,
         jobs=cmd_opts.jobs,
+        compare=cmd_opts.compare,
     )
     x.run()