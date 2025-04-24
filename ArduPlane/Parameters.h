ArduPilot/ardupilot

ArduPlane: changes to support scripted plane follow

ArduPlane/Parameters.h
@@ -571,6 +571,8 @@ public:
     AP_Follow follow;
 #endif
 
+    AP_Int32        guided_update_frequency_limit;
+
     AP_Float        fs_ekf_thresh;
 
     // min initial climb in RTL

SRAravind2410/drone_code

ArduPlane/Parameters.h
@@ -566,6 +566,8 @@ public:
     AP_Follow follow;
 #endif
 
+    AP_Int32        guided_timeout;
+
     AP_Float        fs_ekf_thresh;
 
     // min initial climb in RTL